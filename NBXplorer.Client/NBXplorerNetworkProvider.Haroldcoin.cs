9135742a2cbdc91174fc69dfb9c4b22acfcb37a6
Serhii Prostakov <serhii.prostakov@gmail.com>
2025-04-06 17:33:31 +0300

    Add test Haroldcoin network provider

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Haroldcoin.cs
@@ -0,0 +1,16 @@
+﻿using NBitcoin;
+using NBXplorer.Client;
+
+namespace NBXplorer;
+
+public partial class NBXplorerNetworkProvider
+{
+	private void InitHaroldcoin(ChainName networkType)
+	{
+		Add(new NBXplorerNetwork(Haroldcoin.Instance, networkType)
+		{
+			MinRPCVersion = 70910,
+			CoinType = networkType == ChainName.Mainnet ? new KeyPath("35104'") : new KeyPath("1'")
+		});
+	}
+}
